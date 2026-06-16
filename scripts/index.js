4e5951edc10ef176544313cc1724825c8855dd4d
Eric Malone <Malone1913@gmail.com>
2026-06-16 09:17:18 -0500

    Fix modal layout feedback

--- scripts/index.js
+++ scripts/index.js
@@ -7,14 +7,11 @@ const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 
 const editProfileForm = editProfileModal.querySelector(".modal__form");
-
-editProfileForm.addEventListener("submit", handleEditProfileSubmit);
-
 const nameInput = editProfileModal.querySelector('input[name="name"]');
-
 const descriptionInput = editProfileModal.querySelector(
   'input[name="description"]',
 );
+
 const newPostButton = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
@@ -22,6 +19,32 @@ const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
 const newPostForm = newPostModal.querySelector(".modal__form");
 const imageInput = newPostModal.querySelector('input[name="image-link"]');
 const captionInput = newPostModal.querySelector('input[name="caption"]');
+
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+
+  closeModal(editProfileModal);
+}
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(imageInput.value);
+  console.log(captionInput.value);
+
+  closeModal(newPostModal);
+}
 
 editProfileButton.addEventListener("click", function () {
   nameInput.value = profileTitle.textContent;
@@ -42,30 +65,5 @@ newPostCloseButton.addEventListener("click", function () {
   closeModal(newPostModal);
 });
 
-function handleEditProfileSubmit(evt) {
-  evt.preventDefault();
-
-  profileTitle.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
-
-  closeModal(editProfileModal);
-}
-
-function handleNewPostSubmit(evt) {
-  evt.preventDefault();
-
-  console.log(imageInput.value);
-  console.log(captionInput.value);
-
-  closeModal(newPostModal);
-}
-
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 newPostForm.addEventListener("submit", handleNewPostSubmit);
-
-function openModal(modal) {
-  modal.classList.add("modal_is-opened");
-}
-
-function closeModal(modal) {
-  modal.classList.remove("modal_is-opened");
-}
